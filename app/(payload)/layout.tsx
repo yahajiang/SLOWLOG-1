d145257917d5db98b4487603e9fea905f5ed1525
yahajiang <yahajiang@users.noreply.github.com>
2026-08-31 03:14:25 +0800

    fix: 创建独立serverFunction.ts server action文件，修复Admin面板加载

--- app/(payload)/layout.tsx
+++ app/(payload)/layout.tsx
@@ -1,23 +1,14 @@
 import config from '@payload-config'
 import '@payloadcms/next/css'
-import { handleServerFunctions, RootLayout } from '@payloadcms/next/layouts'
+import { RootLayout } from '@payloadcms/next/layouts'
 import React from 'react'
 
 import { importMap } from './admin/importMap.js'
+import { serverFunction } from './serverFunction'
 import './admin/custom.scss'
 
 type Args = {
   children: React.ReactNode
-}
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const serverFunction = async function (args: any) {
-  'use server'
-  return handleServerFunctions({
-    ...args,
-    config,
-    importMap,
-  })
 }
 
 const Layout = ({ children }: Args) => (
