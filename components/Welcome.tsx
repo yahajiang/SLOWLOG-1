002d8235d6288e8a239b45d1b71417f67111f7bf
yahajiang <yahajiang@users.noreply.github.com>
2026-09-10 23:39:16 +0800

    chore: 移除 Welcome 未使用的 useMemo 导入

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useEffect, useMemo, useState } from "react"
+import { useEffect, useState } from "react"
 import { useLang } from "@/lib/lang-context"
 import { pickTagline } from "@/lib/taglines"
 
